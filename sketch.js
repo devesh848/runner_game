a9822c71736be9e3f709c4e53a8cf0dca0e2e856
devesh848 <67384175+devesh848@users.noreply.github.com>
2020-10-06 21:57:45 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -10,6 +10,7 @@ var score = 0;
 var bumps, bumpIMG;
 var coin, coinIMG;  
 var playerVX;
+var gameOver, reset;
 
 function preload()
 {
@@ -20,41 +21,51 @@ function preload()
   spikeIMG = loadImage("images/spike2.png"); 
   playerStill = loadImage("images/boy2.gif");  
   bumpIMG = loadImage("images/bumps.png");
-  coinIMG = loadImage("images/coin4.png");
+  coinIMG = loadImage("images/coin4.png"); 
+  gameOverIMG = loadImage("images/gameOver.png"); 
+  resetIMG = loadImage("images/restart.png");
 }
 
 function setup() {
     
   bg = createSprite(600,300,10,10); 
   bg.addImage(backgroundIMG);  
-  bg.scale = 2.9; 
-  bg.velocityX = -5;
+  bg.scale = 2.9;
+ 
   createCanvas(1200, 700);
   player = createSprite(500,580);  
   player.debug = true; 
   player.setCollider("circle", 0,0,200); 
   player.addAnimation("running",playerRunning); 
   player.scale = 0.2;    
-  player.velocityX = 0.5;
+  
 
   ground = createSprite(400,680,800,20); 
   ground.addImage(groundIMG);   
   ground.debug = true;
   ground.setCollider("rectangle",0,0,800,200);
   ground.scale = 0.3;   
-  ground.velocityX = -5; 
-  ground.x = 600; 
+  
+  ground.x = 700; 
 
   invisibleGround = createSprite(400,590,800,20);  
   invisibleGround.visible = false;  
-  invisibleGround.velocityX = -3; 
+  
   invisibleGround.x =500;
 
   boulder = createSprite(100,550); 
   boulder.addImage(boulderIMG);   
   boulder.debug = true;
   boulder.scale = 0.2; 
-  boulder.velocityX = 0.5;
+   
+
+  gameOver = createSprite(600,300);
+  gameOver.addImage(gameOverIMG);  
+  gameOver.visible = false;
+
+  reset = createSprite(600,350); 
+  reset.addImage(resetIMG); 
+  reset.visible = false;
 
   spikeGroup = createGroup();  
   bumpGroup = createGroup();
@@ -67,24 +78,29 @@ function draw() {
   background(0);   
   camera.position.x = player.x;  
   camera.position.y = 350;
+  bg.x = camera.position.x; 
+  gameOver.x = camera.position.x;
+  reset.x = camera.position.x; 
+  
    
   
 
   if(gameState ===1){
-  
+  player.velocityX = 0.5; 
+  boulder.velocityX = 0.5; 
+  ground.velocityX = -5; 
+  invisibleGround.velocityX = -3; 
   if(ground.x<0){ 
     //ground.x = ground.width/2;
-    ground.x = 600;
+    ground.x = 700;
   }  
   //console.log(invisibleGround.x); 
-  console.log(player.y);
+
   
   /*if(invisibleGround.x<200){
     invisibleGround.x = 600;
   }*/   
-  if(bg.x < 230){
-    bg.x = 600;
-  }
+    
 
   invisibleGround.x = player.x; 
 
@@ -128,22 +144,37 @@ if(boulder.isTouching(spikeGroup)){
 } else if(gameState === 0){ 
   ground.velocityX = 0; 
   spikeGroup.setVelocityXEach(0); 
-  spikeGroup.setLifetimeEach(-1); 
-  bg.velocityX = 0;  
+  spikeGroup.setLifetimeEach(-1);  
+  coinGroup.setVelocityXEach(0);
+  //bg.velocityX = 0;  
   player.velocityY= 0;    
   player.velocityX = 0;
-  boulder.velocityX = 0; 
+  boulder.velocityX = 0;  
   bumpGroup.setVelocityXEach(0);
   player.addImage(playerStill);
+  gameOver.visible = true;
+  reset.visible = true; 
+  
 
 } 
+if (mousePressedOver(reset)){
+  gameState = 1;  
+  gameOver.visible = false;
+  reset.visible = false;  
+  spikeGroup.destroyEach();
+  bumpGroup.destroyEach();  
+  coinGroup.destroyEach();
+  score = 0;
+  
+
+}
 
   player.collide(invisibleGround);  
   boulder.collide(ground);
   drawSprites(); 
   fill("white"); 
   textSize(25);
-  text("score : " + score, 1000,100); 
+  text("score : " + score, player.x +300,100); 
   stroke(10);
   //add styles here
   
@@ -179,6 +210,7 @@ function spawnCoins(){
     coin.addImage(coinIMG); 
     coin.scale = 0.2;  
     coin.velocityX = -3;  
+    coin.lifetime = 240;
     coinGroup.add(coin);
     
     
